4d4e73d82e36130b37a31530773020a47f53c2a3
arifachratu <arifachqueen@gmail.com>
2024-09-02 19:49:55 +0700

    Praktikum 4D - Membuat Class dari Class Diagram

--- ketiga.php
+++ ketiga.php
@@ -1,81 +1,92 @@
 <?php
 
-class Lingkaran
-{
-    const PHI = 3.14;
-    public $jari_jari;
+class Author {
+    public $name;
+    public $description;
 
-    public function __construct($jari_jari) {
-        $this->jari_jari = $jari_jari;
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
     }
 
-    public function luas() : float {
-        return self::PHI * $this->jari_jari * $this->jari_jari;
-    }
-
-    public function keliling() : float {
-        return 2 * self::PHI * $this->jari_jari;
-    }
-}
-
-class Bola
-{
-    const PHI = 3.14;
-    public $jari_jari;
-
-    public function __construct($jari_jari) {
-        $this->jari_jari = $jari_jari;
-    }
-
-    public function volume() : float {
-        return (4 / 3) * self::PHI * pow($this->jari_jari, 3);
+    public function show($type): array {
+        if ($type === 'name') {
+            return ['name' => $this->name];
+        } else if ($type === 'description') {
+            return ['description' => $this->description];
+        }
+        return [];
     }
 }
 
-class Tabung
-{
-    const PHI = 3.14;
-    public $jari_jari;
-    public $tinggi;
+class Publisher {
+    public $name;
+    public $address;
+    private $phone;
 
-    public function __construct($jari_jari, $tinggi) {
-        $this->jari_jari = $jari_jari;
-        $this->tinggi = $tinggi;
+    public function __construct($name, $address) {
+        $this->name = $name;
+        $this->address = $address;
     }
 
-    public function volume() : float {
-        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
-    }
-}
-
-class Kerucut
-{
-    const PHI = 3.14;
-    public $jari_jari;
-    public $tinggi;
-
-    public function __construct($jari_jari, $tinggi) {
-        $this->jari_jari = $jari_jari;
-        $this->tinggi = $tinggi;
+    public function setPhone(int $phone): void {
+        $this->phone = $phone;
     }
 
-    public function volume() : float {
-        return (1 / 3) * self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
+    public function getPhone(): int {
+        return $this->phone;
     }
 }
 
-// Membuat objek dengan constructor
+class Book {
+    public $ISBN;
+    public $title;
+    public $description;
+    public $category;
+    public $language;
+    public $numberOfPage;
+    public $author;
+    public $publisher;
 
-$lingkaran = new Lingkaran(10);
-echo "Luas lingkaran dengan jari-jari {$lingkaran->jari_jari} cm adalah " . $lingkaran->luas() . " cm²\n";
-echo "Keliling lingkaran dengan jari-jari {$lingkaran->jari_jari} cm adalah " . $lingkaran->keliling() . " cm\n";
+    public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher) {
+        $this->ISBN = $ISBN;
+        $this->title = $title;
+        $this->description = $description;
+        $this->category = $category;
+        $this->language = $language;
+        $this->numberOfPage = $numberOfPage;
+        $this->author = $author;
+        $this->publisher = $publisher;
+    }
 
-$bola = new Bola(10);
-echo "Volume bola dengan jari-jari {$bola->jari_jari} cm adalah " . $bola->volume() . " cm³\n";
+    public function showAll(): array {
+        return [
+            'ISBN' => $this->ISBN,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+            'language' => $this->language,
+            'numberOfPage' => $this->numberOfPage,
+            'author' => $this->author,
+            'publisher' => $this->publisher,
+        ];
+    }
 
-$tabung = new Tabung(10, 20);
-echo "Volume tabung dengan jari-jari {$tabung->jari_jari} cm dan tinggi {$tabung->tinggi} cm adalah " . $tabung->volume() . " cm³\n";
+    public function detail($ISBN): array {
+        if ($this->ISBN === $ISBN) {
+            return $this->showAll();
+        }
+        return [];
+    }
+}
 
-$nasi_tumpeng = new Kerucut(4, 10);
-echo "Volume kerucut nasi tumpeng dengan jari-jari {$nasi_tumpeng->jari_jari} cm dan tinggi {$nasi_tumpeng->tinggi} cm adalah " . $nasi_tumpeng->volume() . " cm³\n";
+// Contoh penggunaan
+$author = new Author("Darwis", "dikenal dengan nama pena Tere Liye adalah penulis dan akuntan berkebangsaan Indonesia");
+$publisher = new Publisher("Republika", "Jakarta");
+$publisher->setPhone(123456789);
+
+$book = new Book(9780747532743, "Hujan", "menceritakan tentang cinta, tentang persahabatan, tentang perpisahan, tentang melupakan, dan tentang hujan", "Fantasi", "jakarta", 223, $author->name, $publisher->name);
+
+print_r($book->showAll());
+print_r($book->detail(9780747532743));
 ?>
