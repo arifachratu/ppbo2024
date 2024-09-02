0da81bbc83b0e99c00d47b2e4596b4d12ab0ecac
arifachratu <arifachqueen@gmail.com>
2024-09-02 18:15:30 +0700

    “Praktikum 2b - Menggunakan Fungsi Bawaan

--- inputoutput.php
+++ inputoutput.php
@@ -1,3 +1,16 @@
 <?php
+date_default_timezone_set("Asia/Jakarta");
 $nama = readline('Masukkan nama Anda: ');
-echo "Halo {$nama}";
+$jam = date('H');
+
+if ($jam >= 5 && $jam < 11) {
+    $salam = "Selamat pagi, ";
+} elseif ($jam >= 11 && $jam < 15) {
+    $salam = "Selamat siang, ";
+} elseif ($jam >= 15 && $jam < 19) {
+    $salam = "Selamat sore, ";
+} else {
+    $salam = "Selamat malam, ";
+}
+
+echo "{$salam} {$nama}, sekarang pukul " . date('H:i') . "\n";
